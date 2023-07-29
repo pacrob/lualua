98a0ba311e0d43d5fac9afd51c20900a8ab597c6
pacrob <pacrob@protonmail.com>
2023-07-29 13:59:31 -0600

    first commit

---
+++ test1.lua
@@ -0,0 +1,18 @@
+#!/usr/bin/lua
+_PROMPT=" cool>> "
+
+print("hello meow")
+
+--[[
+this is a block comment
+it goes as long as I want
+it to and ends when it
+hits double closing
+square brackets
+--]]
+
+meow = "cat"
+print(meow)
+bob = 10
+
+print(arg[1])
